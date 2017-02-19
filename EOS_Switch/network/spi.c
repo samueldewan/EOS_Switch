b3a65a17dbcd28d769e78172e2aa4857d4450d26
Samuel Dewan <samdewan@me.com>
2017-02-19 14:51:37 -0500

    Created network directory with spi.c and spi.h, updated makefile.

---
+++ EOS_Switch/network/spi.c
@@ -0,0 +1,9 @@
+//
+//  spi.c
+//  EOS_Switch
+//
+//  Created by Samuel Dewan on 2017-02-19.
+//  Copyright © 2017 Samuel Dewan. All rights reserved.
+//
+
+#include "spi.h"
---
+++ EOS_Switch/network/spi.h
@@ -0,0 +1,14 @@
+//
+//  calibration.h
+//  DMX-ISPLIT
+//
+//  Created by Samuel Dewan on 2016-10-09.
+//  Copyright © 2016 Samuel Dewan. All rights reserved.
+//
+
+#ifndef debounce_h
+#define debounce_h
+
+
+
+#endif /* debounce_h */
